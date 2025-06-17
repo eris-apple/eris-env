810023cfe3b5d5b2c1f396a0025dd68bd85238e7
yushka_sidra <memusins@gmail.com>
2025-06-17 15:54:18 +0300

    feat: fallback

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ export class ErisEnvLoader {
     else this.logger = false;
   }
 
-  private logging(message: string, method: 'error' | 'debug'): void {
+  private logging(message: string, method: 'error' | 'warning' | 'debug'): void {
     if (this.logger instanceof ErisLogger) this.logger[method]({title: "ENV LOADER", message, error: method === 'error' ? new Error(message) : {}}, );
   }
 
@@ -31,6 +31,28 @@ export class ErisEnvLoader {
     if (!value) {
       this.logging(`The ${name} environment variable is not declared`, 'error');
       throw new Error(`The ${name} environment variable is not declared`);
+    }
+
+    switch (type) {
+      case 'boolean':
+        this.logging(`The ${name} environment variable has been initialized`, 'debug');
+        return (value === 'true') as EnvUtilTypes[T];
+      case 'number':
+        this.logging(`The ${name} environment variable has been initialized`, 'debug');
+        return Number(value) as EnvUtilTypes[T];
+      default:
+        this.logging(`The ${name} environment variable has been initialized`, 'debug');
+        return value as EnvUtilTypes[T];
+    }
+  }
+
+  getEnvWithFallback<T extends EnvUtilType>(type: T, fallback: T, ...names: string[]): EnvUtilTypes[T] {
+    const name = names.join('_');
+    let value = process.env[name];
+
+    if (!value) {
+      this.logging(`The ${name} environment variable is not declared, used fallback`, 'warning');
+      value = fallback
     }
 
     switch (type) {
